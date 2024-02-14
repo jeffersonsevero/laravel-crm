135afa9035c853103b760926be9c6fcf6a3731d8
Jeff <jeffersonsevero08@gmail.com>
2024-02-14 20:34:34 -0300

    Be able to login

---
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Livewire\Auth\Login;
+use App\Models\User;
+use Livewire\Livewire;
+
+it('renders successfully', function () {
+    Livewire::test(Login::class)
+        ->assertOk();
+});
+
+it('should be able to login', function () {
+
+    $user = User::query()->create([
+        'name'     => 'John',
+        'email'    => 'j@j.com',
+        'password' => 'password',
+    ]);
+
+    Livewire::test(Login::class)
+        ->set('email', 'j@j.com')
+        ->set('password', 'password')
+        ->call('tryToLogin')
+        ->assertHasNoErrors()
+        ->assertRedirect(route('dashboard'));
+
+    expect(auth()->check())->toBeTrue()
+        ->and(auth()->user()->id)->toBe($user->id);
+
+});
